98b00596b8b24886b7b1c30e9c55af862e5723cf
jimmys124 <jimmyssantos07@gmail.com>
2025-06-19 22:52:18 +0000

    criei o printf

--- Xadrez.c
+++ Xadrez.c
@@ -3,4 +3,26 @@
 int main(){
     printf("Desafio Xadrez!\n");
     
+    printf("Estado:\n");
+    scanf("Char alagoas.\n");
+
+    printf("Codigo da carta:\n");
+    scanf("Char[] A01,B02,C03,D04,F05.\n");
+
+    printf("Nome da cidade:\n");
+    scanf("Char[] maceio.\n");
+
+    printf("Populaçao,:\n");
+    scanf("int 2.000.000.\n");
+
+    printf("Area em km²:\n");
+    scanf("Float 1.000.000.\n");
+
+    printf("Pib:\n");
+    scanf("Float 10.000.000.\n");
+
+    printf("Numeros de pontos turisticos:\n");
+    scanf("int 20.\n");
+
+
 }
