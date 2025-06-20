5d4b65668382519fb405eab6a847e2aa0584ea03
jimmys124 <jimmyssantos07@gmail.com>
2025-06-20 00:54:29 +0000

    NOVO COMMIT 01

--- Xadrez.c
+++ Xadrez.c
@@ -4,25 +4,25 @@ int main(){
     printf("Desafio Xadrez!\n");
     
     printf("Estado:\n");
-    scanf("Char alagoas.\n");
+    scanf("%f &alagoas\n");
 
     printf("Codigo da carta:\n");
-    scanf("Char[] A01,B02,C03,D04,F05.\n");
+    scanf("%s &A01,B02,C03,D04,F05\n");
 
     printf("Nome da cidade:\n");
-    scanf("Char[] maceio.\n");
+    scanf("%s &maceio\n");
 
     printf("Populaçao,:\n");
-    scanf("int 2.000.000.\n");
+    scanf("%s &2.000.000.\n");
 
     printf("Area em km²:\n");
-    scanf("Float 1.000.000.\n");
+    scanf("%s &1.000.000\n");
 
     printf("Pib:\n");
-    scanf("Float 10.000.000.\n");
+    scanf("%s &10.000.000\n");
 
     printf("Numeros de pontos turisticos:\n");
-    scanf("int 20.\n");
+    scanf("%s &20\n");
 
-
+    return 0;
 }
